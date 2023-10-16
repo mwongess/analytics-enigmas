d963e345ed14c4ff462c2ab8999ac98a08102809
mwongess <amosmwongelah@gmail.com>
2023-10-16 21:34:14 +0300

    calculate area and perimeter

--- area_or_peri.py
+++ area_or_peri.py
@@ -1,3 +1,4 @@
+import math
 from tabulate import tabulate
 
 cols = ["Figures"]
@@ -6,25 +7,24 @@ figures = [["1. cirlce"],["2. rectangle"], ["3. right-angled triangle"]]
 def calc_area_or_peri():
     print(tabulate(figures, headers=cols))
     
-    choice = input("Please chooce the type of the figure")
+    choice = int(input("Please chooce the type of the figure:"))
     if choice in [1, 2, 3]:
         if choice == 1:
             # Circle
-            radius = float(input("Please enter radius"))
-            area = ""
-            perimeter = ""
+            # area = pi * 
+            radius = float(input("Please enter radius:"))
+            area = math.pi * (radius ** 2)
+            perimeter = 2 * math.pi * radius
         elif choice == 2:
-            # Rectangle
-            length = float(input("Please enter radius"))
-            width = float(input("Please enter radius"))
-            area = ""
-            perimeter = ""
+            length = float(input("Please enter the rectangle length:"))
+            width = float(input("Please enter the rectangle width:"))
+            area = length * width
+            perimeter = 2 * (length + width)
         elif choice == 3:
-            # Right-angled triangle
-            base = float(input("Please enter radius"))
-            height = float(input("Please enter radius"))
-            area = ""
-            perimeter = ""
+            base = float(input("Please enter the triangle base:"))
+            height = float(input("Please enter the triangle height:"))
+            area = 0.5 * base * height
+            perimeter = base + height + math.sqrt(base**2 + height**2)
 
 if __name__ == "__main__":
     calc_area_or_peri()
