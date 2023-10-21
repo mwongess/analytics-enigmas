6e684c503c162040022b884a93083aebc456e97a
mwongess <amosmwongelah@gmail.com>
2023-10-21 12:20:19 +0300

    binary search

--- algo/binary_search.py
+++ algo/binary_search.py
@@ -1,27 +1,24 @@
-def binarySearch(array, x, low, high):
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2  # Calculate the middle index
+        
+        if arr[mid] == target:
+            return mid  # Element found, return its index
+        elif arr[mid] < target:
+            left = mid + 1  # Target is in the right half
+        else:
+            right = mid - 1  # Target is in the left half
+    
+    return -1  # Element is not in the array
 
-	while low <= high:
-
-		mid = low + (high - low)//2
-
-		if array[mid] == x:
-			return mid
-
-		elif array[mid] < x:
-			low = mid + 1
-
-		else:
-			high = mid - 1
-
-	return -1
-
-
-array = [2, 4, 5, 17, 14, 7, 11, 22]
-x = 22
-
-result = binarySearch(array, x, 0, len(array)-1)
+# Example usage:
+my_list = [11, 12, 22, 25, 34, 64, 90]
+target = 11
+result = binary_search(my_list, target)
 
 if result != -1:
-	print(str(result))
+    print(f"Element {target} found at index {result}.")
 else:
-	print("Not found")
+    print(f"Element {target} not found in the list.")
