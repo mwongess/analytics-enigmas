7d73f0e2e4b60c4ed9803e7bfde110fd2d1723fc
mwongess <amosmwongelah@gmail.com>
2023-10-21 12:41:32 +0300

    linear search

--- algo/linear_search.py
+++ algo/linear_search.py
@@ -1,16 +1,15 @@
-def linearSearch(array, n, x):
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  # Element found, return its index
+    return -1  # Element is not in the array
 
-	for i in range(0, n):
-		if (array[i] == x):
-			return i
-	return -1
+# Example usage:
+my_list = [11, 12, 22, 25, 34, 64, 90]
+target = 25
+result = linear_search(my_list, target)
 
-
-array = [24, 41, 31, 11, 9]
-x = 11
-n = len(array)
-result = linearSearch(array, n, x)
-if(result == -1):
-	print("Element not found")
+if result != -1:
+    print(f"Element {target} found at index {result}.")
 else:
-	print("Element is Present at Index: ", result)
+    print(f"Element {target} not found in the list.")
